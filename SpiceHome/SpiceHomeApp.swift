47d93883e9c57058927db480505b85c0e9dbc35d
Anoop Jose <anoop@flutterly.co.uk>
2021-09-11 15:28:50 +0100

    Initial Commit

---
+++ SpiceHome/SpiceHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceHomeApp.swift
+//  SpiceHome
+//
+//  Created by Anoop Jose on 11/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpiceHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
